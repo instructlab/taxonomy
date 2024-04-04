682660cb4ae3baa8f72078542ae3e3ab878550f4
tomcli <tommy.chaoping.li@ibm.com>
2024-04-03 18:04:35 -0700

    update cos path to the new bucket
    Signed-off-by: tomcli <tommy.chaoping.li@ibm.com>

--- synthetic_data/generate_readme.sh
+++ synthetic_data/generate_readme.sh
@@ -1,4 +1,4 @@
-export s3_path=https://instruct-lab-host.s3.us-east.cloud-object-storage.appdomain.cloud
+export s3_path=https://instruct-lab.s3.us-east.cloud-object-storage.appdomain.cloud
 export files=$(find compositional_skills -type f -exec echo $s3_path/{} \;)
 export model_filename=mistrial-7b
 
@@ -7,15 +7,15 @@ echo '
 # Instruct Lab Synthetic Data
 
 Synthetic data from the latest release are hosted on the Cloud at:
-https://instruct-lab-host.s3.us-east.cloud-object-storage.appdomain.cloud/
+https://instruct-lab.s3.us-east.cloud-object-storage.appdomain.cloud/
 
 > **_NOTE:_** Currently the full synthetic data is not yet available, it will be ready during the next model release.
 
 To download a particular set of synthetic data, use the above link + the directory path + model filename + file extension as the URL link. Currently, the two file extensions are `.parquet` for full synthetic data and `.preview.json` to preview the first 10 examples in JSON. For example:
 
 - The synthetic data for `compositional_skills/extraction/abstractive/abstract` using the `mistrial-7b` model:
-  - Full synthetic data: https://instruct-lab-host.s3.us-east.cloud-object-storage.appdomain.cloud/compositional_skills/extraction/abstractive/abstract/mistrial-7b.parquet
-  - Preview synthetic data in JSON: https://instruct-lab-host.s3.us-east.cloud-object-storage.appdomain.cloud/compositional_skills/extraction/abstractive/abstract/mistrial-7b.preview.json
+  - Full synthetic data: https://instruct-lab.s3.us-east.cloud-object-storage.appdomain.cloud/compositional_skills/extraction/abstractive/abstract/mistrial-7b.parquet
+  - Preview synthetic data in JSON: https://instruct-lab.s3.us-east.cloud-object-storage.appdomain.cloud/compositional_skills/extraction/abstractive/abstract/mistrial-7b.preview.json
 
 The preview JSON file can be opened as it is. To open the parquet file with full synthetic data, use the following commands to install the Python libraries and load the parquet file into the Pandas data frame:
 ```bash
